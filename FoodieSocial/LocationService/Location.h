7d3ddb5141568812d024d611e36d1c2245cca2b8
Xinjiang Shao <shaoxinjiang@gmail.com>
2012-10-18 15:56:10 -0500

    Ignore Binary generated by Xcode during compiling; Added Location Class.

---
+++ FoodieSocial/LocationService/Location.h
@@ -0,0 +1,13 @@
+//
+//  Location.h
+//  FoodieSocial
+//
+//  Created by shaoxinjiang on 10/18/12.
+//  Copyright (c) 2012 Xinjiang Shao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Location : NSObject
+
+@end
---
+++ FoodieSocial/LocationService/Location.m
@@ -0,0 +1,13 @@
+//
+//  Location.m
+//  FoodieSocial
+//
+//  Created by shaoxinjiang on 10/18/12.
+//  Copyright (c) 2012 Xinjiang Shao. All rights reserved.
+//
+
+#import "Location.h"
+
+@implementation Location
+
+@end
